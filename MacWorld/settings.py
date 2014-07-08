051f328ec063d933bfff906efd9e767e05eed77f
Alan Morales <alanrmorales@gmail.com>
2014-07-08 08:45:12 -0500

    gitignore for gmail username and password

--- MacWorld/settings.py
+++ MacWorld/settings.py
@@ -85,5 +85,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yourgmailaccount@gmail.com'
-EMAIL_HOST_PASSWORD = 'yourgmailpassword'
