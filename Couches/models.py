2d0a4123917d67cb00c8d345c46536ec83f73604
Alan Morales <alanrmorales@gmail.com>
2014-07-09 21:30:56 -0500

    added graduation year field

--- Couches/models.py
+++ Couches/models.py
@@ -12,22 +12,20 @@ class UserProfile(UserenaBaseProfile):
                                 verbose_name=_('user'),
                                 related_name='my_profile')
 
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
     description = models.CharField(max_length=300, blank=True)
     contact_information = models.CharField(max_length=300, blank=True) # extra contact information that the user wishes to include
+    graduation_year = models.IntegerField(null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return u'%s %s' % (self.first_name, self.last_name)
-
-
+    
 class Location(models.Model):
     user = models.ForeignKey(User)
     available = models.BooleanField() # Is this location available to couchsurf?
-
+    
     latitude = models.CharField(max_length=30, validators=[RegexValidator(regex='^[-+]?[0-9]*\.?[0-9]+$'),]) # floating point validator
     longitude = models.CharField(max_length=30, validators=[RegexValidator(regex='^[-+]?[0-9]*\.?[0-9]+$'),]) # floating point validator
 
