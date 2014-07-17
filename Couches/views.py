c3345a66b1d5a95170f3a27e2a91947b968d86f9
Connor Boyle <cboyle@macalester.edu>
2014-07-16 20:07:56 -0700

    Added a little more functionality.

--- Couches/views.py
+++ Couches/views.py
@@ -1,8 +1,8 @@
-from Couches.forms import LoginForm
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse
-from django.views.generic import FormView
+from django.views.generic import FormView, RedirectView
 from django.views.generic.base import TemplateView
 
 
@@ -11,13 +11,22 @@ class HomeView(TemplateView):
 
 
 class LoginView(FormView):
-    form_class = LoginForm
+    form_class = AuthenticationForm
     template_name = 'login.html'
     success_url = reverse_lazy('home')
 
-    def form_valid(self, form):
-        authenticate(username=form['username'], password=form['password'])
+    def form_valid(self, form):  # called if the entered username and password are valid
+        login(self.request, form.get_user())
         return super(LoginView, self).form_valid(form)
 
-    def form_invalid(self, form):
+    def form_invalid(self, form):  # called when the entered username and password are invalid
         return HttpResponse("Oh my, you've really gone and done it now.")
+
+
+class LogoutView(RedirectView):
+    permanent = False
+    url = reverse_lazy('home')
+
+    def dispatch(self, request, *args, **kwargs):
+        logout(request)
+        return super(LogoutView, self).dispatch(request, *args, **kwargs)
--- MacWorld/urls.py
+++ MacWorld/urls.py
@@ -1,4 +1,4 @@
-from Couches.views import LoginView, HomeView
+from Couches.views import HomeView, LoginView, LogoutView
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -7,5 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'^login/', LoginView.as_view(), name='login'),
+    url(r'^logout/', LogoutView.as_view(), name='logout'),
     url(r'^$', HomeView.as_view(), name='home')
 )
