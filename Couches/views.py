edf6727956ac8e5f700203bcba2582ca33349dab
Connor Boyle <cboyle@macalester.edu>
2014-07-23 18:27:43 -0700

    Crude permissions protection on LocationUpdate and LocationDelete. Even cruder DetailView for Location.

--- Couches/views.py
+++ Couches/views.py
@@ -1,22 +1,40 @@
 from Couches.models import Location
+from django.core.exceptions import PermissionDenied
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import UpdateView
-from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+
+
+class LocationDetail(DetailView):
+    model = Location
+    template_name = 'location_detail.html'
 
 
 class LocationCreate(CreateView):
     model = Location
     template_name = 'location_edit.html'
-    success_url = reverse_lazy('couches-home')
 
 
 class LocationUpdate(UpdateView):  # TODO: this should have a custom widget that uses a map
     model = Location
     template_name = 'location_edit.html'
-    success_url = reverse_lazy('couches-home')
+
+    def get_object(self, queryset=None):
+        obj = super(LocationUpdate, self).get_object(queryset)
+        if obj.user != self.request.user:
+            raise PermissionDenied()
+        else:
+            return obj
 
 
 class LocationDelete(DeleteView):
     model = Location
     template_name = 'location_delete.html'
     success_url = reverse_lazy('couches-home')
+
+    def get_object(self, queryset=None):
+        obj = super(LocationDelete, self).get_object(queryset)
+        if obj.user != self.request.user:
+            raise PermissionDenied()
+        else:
+            return obj
--- MacWorld/urls.py
+++ MacWorld/urls.py
@@ -1,4 +1,4 @@
-from Couches.views import LocationUpdate, LocationCreate, LocationDelete
+from Couches.views import LocationUpdate, LocationCreate, LocationDelete, LocationDetail
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from Couches.forms import SignupFormExtra
@@ -7,19 +7,17 @@ from Couches.forms import EditProfileFormExtra
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    (r'^Couches/signup/$',
-    'userena.views.signup',
-    {'template_name':'signup.html','signup_form': SignupFormExtra}),
-    url(r'^Couches/(?P<username>[\.\w-]+)/edit/$',
-    'userena.views.profile_edit',
-    {'template_name':'profile_form.html','edit_profile_form': EditProfileFormExtra}),
-    url(r'^Couches/(?P<username>[\.\w-]+)/$',
-    'userena.views.profile_detail',
-    {'template_name':'profile_detail.html'}),
-    url(r'^Couches/', include('userena.urls')),
-    url(r'^messages/', include('userena.contrib.umessages.urls')),
-    url(r'^location_delete/(?P<pk>\d+)/$', LocationDelete.as_view(), name='couches-location-delete'),
-    url(r'^location_update/(?P<pk>\d+)/$', LocationUpdate.as_view(), name='couches-location-update'),
-    url(r'^location_create/$', LocationCreate.as_view(), name='couches-location-create'),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^Couches/signup/$', 'userena.views.signup',
+                           {'template_name': 'signup.html', 'signup_form': SignupFormExtra}),
+                       url(r'^Couches/(?P<username>[\.\w-]+)/edit/$', 'userena.views.profile_edit',
+                           {'template_name': 'profile_form.html', 'edit_profile_form': EditProfileFormExtra}),
+                       url(r'^Couches/(?P<username>[\.\w-]+)/$', 'userena.views.profile_detail',
+                           {'template_name': 'profile_detail.html'}),
+                       url(r'^Couches/', include('userena.urls')),
+                       url(r'^messages/', include('userena.contrib.umessages.urls')),
+                       url(r'^location_detail/(?P<pk>\d+)/$', LocationDetail.as_view(), name='couches-location-detail'),
+                       url(r'^location_delete/(?P<pk>\d+)/$', LocationDelete.as_view(), name='couches-location-delete'),
+                       url(r'^location_update/(?P<pk>\d+)/$', LocationUpdate.as_view(), name='couches-location-update'),
+                       url(r'^location_create/$', LocationCreate.as_view(), name='couches-location-create'),
 )
