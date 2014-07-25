2630f6fd1e70b7845bd8b316e3183d320b169880
Connor Boyle <cboyle@macalester.edu>
2014-07-24 23:20:44 -0700

    Removed some unnecessary comments

--- Couches/forms.py
+++ Couches/forms.py
@@ -3,18 +3,12 @@ from django.forms import ModelForm
 
 
 class CouchesProfileForm(ModelForm):
-    """
-    Form for creating or editing "Couches" profile
-    """
     class Meta:
         model = CouchesProfile
         fields = ['description', 'contact_information', 'graduation_year']
 
 
 class CouchForm(ModelForm):
-    """
-    Form for creating or editing Couches
-    """
     class Meta:
         model = Couch
         fields = ['owner', 'lon', 'lat']
