59ce23a7d2304f422e2afc46b1d4e441f79be2fb
Connor Boyle <cboyle@macalester.edu>
2014-07-24 17:41:48 -0700

    A user who attempts to access his CouchesProfile before it's created is redirected to the Create page, the way CouchesProfile's are created (specifically, how the user they're attached to is determined) is a little cleaner.

--- Couches/views.py
+++ Couches/views.py
@@ -3,7 +3,9 @@ from Couches.models import CouchesProfile, Couch
 from django.core.urlresolvers import reverse_lazy
 from django.http.response import HttpResponse, Http404
 from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
 from django.views.generic import DetailView, CreateView, UpdateView, ListView
+from guardian.decorators import permission_required_or_403
 
 
 class CouchesHomeView(ListView):
@@ -23,16 +25,19 @@ class ProfileDetailView(ProfileByUsernameMixin, DetailView):
     context_object_name = 'user_profile'
     template_name = 'user_profile/detail.html'
 
-    '''
     def get(self, request, *args, **kwargs):
         try:
-            return super(ProfileDetailView, self).get(self, request, *args, **kwargs)
-        except Http404:
-            if request.user == self.object.user:
-                return redirect(to=reverse_lazy('couches-profile-create'))
-            else:
-                return super(ProfileDetailView, self).get(self, request, *args, **kwargs)
-    '''
+            return super(ProfileDetailView, self).get(request)
+        except NotImplementedError:
+            return redirect(to=reverse_lazy('couches-profile-create'), permanent=False)
+
+    def get_object(self, queryset=None):
+        try:
+            return super(ProfileDetailView, self).get_object(queryset)
+        except Http404 as error:
+            if self.request.user.username == self.kwargs['username']:
+                raise NotImplementedError()  # FIXME: better exception needed (possibly custom)
+            raise error
 
 
 class ProfileCreateView(ProfileByUsernameMixin, CreateView):
@@ -40,10 +45,11 @@ class ProfileCreateView(ProfileByUsernameMixin, CreateView):
     form_class = CouchesProfileForm
     template_name = 'user_profile/create.html'
 
-    def post(self, request, *args, **kwargs):
-        request.POST = request.POST.copy()
-        request.POST['user'] = request.user.id
-        return super(ProfileCreateView, self).post(request, *args, **kwargs)
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super(ProfileCreateView, self).form_valid(form)
 
 
 class ProfileUpdateView(ProfileByUsernameMixin, UpdateView):
