a9f83f5954ef7b459455515b20747b21470ff433
Connor Boyle <cboyle@macalester.edu>
2014-07-30 18:56:27 -0700

    couch represented as object in CouchUpdateView

--- Couches/views.py
+++ Couches/views.py
@@ -41,6 +41,11 @@ class CouchUpdateView(PermReq, UpdateView):
     form_class = CouchForm
     template_name = 'couch/edit.html'
 
+    def get_context_data(self, **kwargs):
+        context_data = super(CouchUpdateView, self).get_context_data(**kwargs)
+        context_data.update({'couch': self.object})
+        return context_data
+
 
 class CouchDeleteView(PermReq, DeleteView):
     permission_required = 'Couches.delete_couch'
