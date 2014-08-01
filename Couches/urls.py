1bb5ec956af75cf4c0aef9c3a5fbb083d537dd35
Alan Morales <alanrmorales@gmail.com>
2014-08-01 08:19:33 -0400

    ups imports but still not working

--- Couches/urls.py
+++ Couches/urls.py
@@ -1,5 +1,5 @@
 from Couches.api import CouchViewSet
-from Couches.views import CouchesHomeView, CouchCreateView, CouchUpdateView, CouchDeleteView, CouchDetailView
+from Couches.views import CouchesHomeView, CouchCreateView, CouchUpdateView, CouchDeleteView, CouchDetailView, ProfileUpdateView, ProfileDetailView
 from django.conf.urls import patterns, url, include
 from rest_framework import routers
 
