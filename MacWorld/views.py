d9738444f80b654c721895320d4ff4ac9762d27c
Alan Morales <alanrmorales@gmail.com>
2014-08-01 13:55:36 -0400

    added context to show couches on homepage

--- MacWorld/views.py
+++ MacWorld/views.py
@@ -1,9 +1,14 @@
 from django.views.generic import TemplateView
+from Couches.models import Couch
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['couches'] = Couch.objects.all()
+        return context
 
 class AuthHomeView(TemplateView):
     template_name = 'auth/home.html'
