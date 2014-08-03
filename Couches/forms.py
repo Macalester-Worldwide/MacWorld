15a947effaac77c4ba285a5762ad15d3031009e9
Alan Morales <alanrmorales@gmail.com>
2014-08-03 16:04:25 -0400

    made text areas on profile edit

--- Couches/forms.py
+++ Couches/forms.py
@@ -1,13 +1,14 @@
 from Couches.models import User, Couch
 from django import forms
 from django.forms import ModelForm
-from django.forms.widgets import HiddenInput, TextInput
+from django.forms.widgets import HiddenInput, TextInput, Textarea
 
 
 class ProfileForm(ModelForm):
     class Meta:
         model = User
-        fields = ['name', 'description', 'contact_information', 'graduation_year', 'profile_picture']
+        fields = ['name', 'description', 'contact_information', 'graduation_year', 'profile_picture', 'email_visible']
+        widgets = {'description' : Textarea, 'contact_information' : Textarea, }
 
     def signup(self, request, user):
         for field in self.Meta.fields:
